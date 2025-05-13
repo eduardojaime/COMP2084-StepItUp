365677d20276949b16b0960251180aa496b6c48f
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-05-13 11:53:02 -0400

    l2/1 - CategoriesController + Category model

---
+++ StepItUp/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StepItUp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // make a mock list of categories & pass to view for display
+
+
+            return View();
+        }
+    }
+}
