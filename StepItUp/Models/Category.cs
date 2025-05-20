a9442622861b10b6d45800ca65f45cb4cb7ea1e3
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-05-20 12:50:48 -0400

    l3/2 - Product model w/relationship to Category (1 cat / many prods)

--- StepItUp/Models/Category.cs
+++ StepItUp/Models/Category.cs
@@ -4,5 +4,8 @@
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
+
+        // child ref to Product model: 1 category has many products
+        public List<Product>? Products { get; set; }
     }
 }
---
+++ StepItUp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace StepItUp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public string Colour { get; set; }
+        public string? Photo { get; set; }
+
+        // FK => reference to Category a Product belongs to
+        public int CategoryId { get; set; }
+
+        // parent reference to Category
+        public Category Category { get; set; }
+    }
+}
