9a063f8be84aa98ca09dcd18d662807af2c224b0
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-06-03 11:55:00 -0400

    l5/1 - product read & create

--- StepItUp/Controllers/ProductsController.cs
+++ StepItUp/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using StepItUp.Data;
+using StepItUp.Models;
 
 namespace StepItUp.Controllers
 {
@@ -17,17 +19,34 @@ namespace StepItUp.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // fetch product list from db. include join to parent Category
+            var products = _context.Product.OrderBy(p => p.Name).Include(p => p.Category).ToList();
+
+            // load view and pass product list
+            return View(products);
         }
 
+        // GET: load empty product form including dropdown of categories
         public IActionResult Create()
         {
             // fetch list of Categories for parent dropdown list
             var categories = _context.Category.OrderBy(c => c.Name).ToList();
-            ViewBag["CategoryId"] = new SelectList(categories, "CategoryId", "Name");
+            ViewData["CategoryId"] = new SelectList(categories, "CategoryId", "Name");
 
             // show a blank form to enter a new product
             return View();
+        }
+
+        // POST: process create form submission to save new product
+        [HttpPost]
+        public IActionResult Create([Bind("Name,Description,Size,Price,Colour,Photo,CategoryId")] Product product)
+        {
+            // save new product to db
+            _context.Product.Add(product);
+            _context.SaveChanges();
+
+            // redirect to products Index to see updated list
+            return RedirectToAction("Index");
         }
 
         public IActionResult Edit()
