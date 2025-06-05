0e9aeb772595bce07b2734d441f6e9903554a0f2
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-06-05 07:58:47 -0400

    l5/3 - validation rules in product model

--- StepItUp/Models/Product.cs
+++ StepItUp/Models/Product.cs
@@ -1,12 +1,19 @@
-﻿namespace StepItUp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StepItUp.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [MaxLength(3)]
         public string Size { get; set; }
+
+        [Range(0.01,500)]
         public decimal Price { get; set; }
+
         public string Colour { get; set; }
         public string? Photo { get; set; }
 
