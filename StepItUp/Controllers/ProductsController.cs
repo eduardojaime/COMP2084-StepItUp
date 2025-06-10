74900c24d465cc8205e2edf89302d1db254444e0
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-06-10 13:25:05 -0400

    l6/2 - restrict catalog mgmt to Administrators

--- StepItUp/Controllers/ProductsController.cs
+++ StepItUp/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StepItUp.Data;
@@ -6,6 +7,8 @@ using StepItUp.Models;
 
 namespace StepItUp.Controllers
 {
+    // restrict all methods to Administrator role only
+    [Authorize(Roles = "Administrator")]
     public class ProductsController : Controller
     {
         // class-level db conn
