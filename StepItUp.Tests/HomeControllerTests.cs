0dbad197952753a3ce32ce38659c161963c088ea
Eduardo Jaime <eduardojaime86@gmail.com>
2025-07-29 19:53:46 -0400

    l12 Unit Test setup and basic tests

---
+++ StepItUp.Tests/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StepItUp.Controllers;
+
+namespace StepItUp.Tests
+{
+    // Make class public and sealed
+    // Mark it with the TestClass attribute
+    [TestClass]
+    public sealed class HomeControllerTests
+    {
+        // You should have at least one test method per action method in the controller
+        [TestMethod]
+        public void IndexReturnsViewResult()
+        {
+            // Arrange > initialize controller class
+            var controller = new HomeController();
+
+            // Act > call the index() action method
+            var result = controller.Index();
+
+            // Assert > verify that result is of type ViewResult
+            Assert.IsInstanceOfType<ViewResult>(result);
+        }
+    }
+}
---
+++ StepItUp.Tests/Test1.cs
@@ -0,0 +1,24 @@
+﻿namespace StepItUp.Tests
+{
+    [TestClass]
+    // Sealed classes cannot be inherited, which is useful for unit tests
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TwoPlusTwoEqualFour()
+        {
+            // Arrange > prepare test data
+            var a = 2;
+            var b = 2;
+            var expected = 4;
+            var actual = 0; // initialize the variable to hold the result
+
+            // Act > perform the action to be tested or call the method to be tested
+            actual = a + b;
+
+            // Assert > verify the results
+            // Use the Assert class
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
